a239bc2337b459b14aee8164f31783ff8b781722
Valentin BELAUD <valentin@noosys.fr>
2018-06-13 10:01:47 +0200

    Add server.js, config.js, rep api/controller|models|routes

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret' : 'noo$$y$r0ck$',
+    'database' : 'mongodb://127.0.0.1:27017/poc_chat_socket'
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+// Import des package
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var io = require('socket.io')(server);
+var config = require('./config');
+
+// Export des variables
+module.exports.app = app;
+
+// Configuration
+var port = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
+
+// Initialisation de body-parse
+app.user(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Initialisation de morgan
+app.use(morgan('dev'));
+
+// route de base
+app.get('/', function (request, response) {
+    response.send('vide');
+});
+
+// Import des routes
+var routes = require('./api/routes/routes');
+routes(app);
+
+// Démarrage du serveur
+app.listen(port);
+console.log('server started on port : ' + port);
